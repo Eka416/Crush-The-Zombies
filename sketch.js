b030758e34f76f4993404a5a757d03a89c526eaf
Eka416 <87167166+Eka416@users.noreply.github.com>
2021-08-11 01:00:39 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ const Composite = Matter.Composite;
 
 let engine, world
 
-var ground, RWall, LWall, bridge, joinLink, joinPoint
-var zombieImg, axeImg, stoneImg, woodImg, bgImg
+var ground, RWall, LWall, bridge, joinLink, joinPoint, bg
+var zombieImg, axeImg, stoneImg, woodImg, bgImg, zombie, breakButton
 var stones = []
 
 function preload() {
@@ -23,13 +23,13 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   engine = Engine.create();
   world = engine.world;
   frameRate(80);
   ground = new Base(width/2,height-10,width,20,"black", true)
   RWall = new Base(width - 50,height/2 + 50,100,100, "yellow", true)
   LWall = new Base(50,height/2 + 50, 100, 100, "yellow", true)
-  //stones fall under bridge
   bridge = new Bridge(23,{x:-30, y:height/2})
   joinPoint = new Base(width - 50, height/2 + 10, 40, 20, "black", true)
   Composite.add(bridge.body, joinPoint)
@@ -40,16 +40,47 @@ function setup() {
     var stone = new Stone(x,y,80,80)
     stones.push(stone)
   }
+  zombie = createSprite(width/2, height - 110)
+  zombie.addImage("zombie1",zombieImg)
+  zombie.scale = 0.1
+  zombie.velocityX = 10
+
+  breakButton = createButton("hi")
+  breakButton.position(width - 200, height/2 - 50)
+  breakButton.class("breakbutton")
+  breakButton.mouseClicked(handleButtonPress)
+  
+
+  
+  
 }
 
 function draw() {
   background(51);
+  imageMode(CENTER)
+  createEdgeSprites()
+  image(bgImg,width/2,height/2,width,height);
+
   Engine.update(engine);
   ground.display()
   bridge.show()
   RWall.display()
   LWall.display()
+  if (zombie.x > width) {
+    zombie.velocityX = -10
+  }
+  if (zombie.x < 0) {
+    zombie.velocityX = 10
+  }
   for (var stone of stones) {
     stone.display()
   }
+  drawSprites()
+}
+function handleButtonPress() {
+  joinLink.detach()
+  setTimeout(() => {
+    bridge.break()
+  }, 1500);
+  console.log("hi")
 }
